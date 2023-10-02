74236bacfe7bade8f3fefe6a51cf0121ca0d6bb2
Mertcanroc <143512927+Mertcanroc@users.noreply.github.com>
2023-10-02 21:59:02 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+let a = 5;
+let b = 5;
+let result = (a == b);
+console.log(result) ;
+
+ 
+
+let x = 'appel'
+let y = 'sinnasappel';
+let resultaat = x != y;
+console.log (resultaat);
+
+ 
+
+let c = 10;
+let d = 5;
+let sonuc = c > d;
+console.log (sonuc);
+
+ 
+
+let e = 3;
+let f = 7;
+let ef = e > f;
+console.log(ef);
